24bb0f589205f27dcef8424b44cfbea1aed8185b
yujin0408 <aszx5849@naver.com>
2025-07-30 00:57:22 +0900

    feat: 버튼 icon props 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Button from "@/components/common/Button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Button>버튼</Button>
       <Button buttonType="outline">라인</Button>
+      <Button buttonType="secondary" className="w-[277px]">
+        삭제하기
+      </Button>
+      <Button
+        buttonType="secondary"
+        className="text-gray-50 w-[122px] mt-3 rounded-md"
+      >
+        작성 취소
+      </Button>
     </div>
   );
 }
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,14 +1,15 @@
 import cn from "@/utils/cn";
 import { cva } from "class-variance-authority";
-import React, { ButtonHTMLAttributes, ReactElement } from "react";
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
 
 const buttonVariants = cva(
-  "w-full p-4 lg:p-5 rounded-lg text-lg lg:text-xl font-semibold ",
+  "w-full p-4 lg:p-5 rounded-lg text-lg lg:text-xl font-semibold flex items-center justify-center",
   {
     variants: {
       buttonType: {
         primary:
           "bg-orange-300 text-gray-50 hover:bg-orange-200 focus-within:bg-orange-200",
+        secondary: "bg-line-200 text-gray-300",
         outline:
           "border border-orange-300 text-orange-300 hover:border-orange-200 hover:text-orange-200 focus-within-border-orange-200 focus-within-text-orange-200",
       },
@@ -37,16 +38,28 @@ const buttonVariants = cva(
 );
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  buttonType?: "primary" | "outline";
+  children: React.ReactNode;
+  buttonType?: "primary" | "secondary" | "outline";
+  icon?: ReactNode;
 }
 
-function Button({ buttonType = "primary", disabled = false, ...props }: Props) {
+function Button({
+  buttonType = "primary",
+  disabled = false,
+  icon,
+  children,
+  className,
+  ...props
+}: Props) {
   return (
     <button
       disabled={disabled}
-      className={cn(buttonVariants({ buttonType, disabled }))}
+      className={cn(buttonVariants({ buttonType, disabled }), className)}
       {...props}
-    />
+    >
+      {icon && <span className="mr-[0.25rem]">{icon}</span>}
+      {children}
+    </button>
   );
 }
 
